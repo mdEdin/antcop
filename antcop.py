eb87a9276a805d3aa5717a5c2a929b225c1480d8
Edin 🐲 <96621244+mdEdin@users.noreply.github.com>
2024-12-31 01:57:31 +0200

    new file:   antcop.py 	new file:   texts/text1 	new file:   texts/text2 	new file:   texts/text3 	new file:   texts/text4 	new file:   texts/text5

---
+++ antcop.py
@@ -0,0 +1,31 @@
+import os
+import numpy
+
+path = "./texts"
+
+# Find all files in a directory and output their names
+
+def searchFolder(path):
+    if os.path.exists(path):
+        files = os.listdir(path)
+        files.sort()
+        print("The files in the directory: " + path + " are" + "\n")
+        for x in files:
+            print(x)
+    else: print ("There is no folder with the name " + path)
+    print("\n")
+    return files
+
+# This a reference to the list of files found
+
+allFiles = searchFolder(path)
+
+def fileContents():
+    os.chdir(path)
+    print("File Contents\n")
+    for x in allFiles:
+        print(x)
+        file = open(x, "rt")
+        print(file.read() + "\n")
+
+fileContents()
